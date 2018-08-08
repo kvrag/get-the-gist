6af66f4096a3763b941bc7df38d1bc855741a0f4
Kristina Vragovic <k.vragovic@gmail.com>
2018-08-08 11:22:50 -0500

    Add Alamofire pod

---
+++ Pods/Target Support Files/Pods-GetTheGist/Pods-GetTheGist-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetTheGist : NSObject
+@end
+@implementation PodsDummy_Pods_GetTheGist
+@end
---
+++ Pods/Target Support Files/Pods-GetTheGistTests/Pods-GetTheGistTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GetTheGistTests : NSObject
+@end
+@implementation PodsDummy_Pods_GetTheGistTests
+@end
